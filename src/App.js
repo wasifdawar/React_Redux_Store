656360867b26be012264fe8a4a91a06aa8888117
wasifdawar <m.wasifdawar@gmail.com>
2021-11-23 00:28:41 +0500

    Feat : Redux Implementation before setting Api fetch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import Header from "./containers/Header";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ProductListing from "./containers/ProductListing";
+import ProductDetail from "./containers/ProductDetail";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Header />
+        <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" exact component={ProductDetail} />
+          <Route>404 not found!</Route>
+        </Switch>
       </Router>
     </div>
   );
--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import axios from "axios";
+import ProductComponent from "./ProductComponent";
 
 const ProductListing = () => {
+  const products = useSelector((state) => state);
+  const fetchProducts = () => {
+    const response = axios
+      .get("'https://fakestoreapi.com/products")
+      .catch((err) => {
+        console.log("Error Occured", err);
+      });
+    console.log(response.data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products);
+
   return (
-    <div>
-      <h1>ProductListing</h1>
+    <div className="ui grid container">
+      <ProductComponent />
     </div>
   );
 };
