6cce860fc6729bd1983762373826203b0d2e57b8
wasifdawar <m.wasifdawar@gmail.com>
2021-11-24 17:55:35 +0500

    Use params to get ID

--- src/containers/ProductComponent.js
+++ src/containers/ProductComponent.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import {Link} from "react-router-dom";
+
 
 const ProductComponent = () => {
   const products = useSelector((state) => state.allProducts.products);
@@ -7,6 +9,7 @@ const ProductComponent = () => {
     const { id, title, image, price, category } = product;
     return (
       <div className="four wide column" key={id}>
+        <Link to={`/product/${id}`}>
         <div className="ui link cards">
           <div className="card">
             <div className="image">
@@ -19,6 +22,7 @@ const ProductComponent = () => {
             </div>
           </div>
         </div>
+        </Link>
       </div>
     );
   });
